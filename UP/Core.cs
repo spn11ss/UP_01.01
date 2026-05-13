0a65b11ed1669d1edc848230ebadd1ace64a13ac
ника <stepanecnikusa@gmail.com>
2026-05-13 15:49:17 +0300

    Подключение базы данных, ссылок и создание класса Core.cs

---
+++ UP/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP
+{
+    internal class Core
+    {
+        public static UP_GordovEntities Context = new UP_GordovEntities();
+    }
+}
